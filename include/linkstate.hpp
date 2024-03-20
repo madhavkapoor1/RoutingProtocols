c85ac08cbcf6b35554ca5e2fbe2d4e5cc2a0ec8b
Lakshya013 <Lakshya5@students.ubc.ca>
2024-03-19 23:39:43 -0700

    complete git add .

---
+++ include/linkstate.hpp
@@ -0,0 +1,21 @@
+#ifndef LINKSTATE_HPP
+#define LINKSTATE_HPP
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <climits>  
+#include <sstream>
+#include <fstream>
+#include <queue>
+#include <utility>
+#include <algorithm>
+
+struct Node{
+    int destination;
+    int nextHop;
+    int cost;
+};
+
+#endif // LINKSTATE_HPP
